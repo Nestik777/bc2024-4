b8b134b29df4320e676639d5e8b7f1e1d4b42506
nestor bc2024-4 <nestor.kuleba189@gmail.com>
2024-10-15 20:47:42 +0300

    add get

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
 const { Command } = require('commander');
-
-// Налаштування параметрів командного рядка
 const program = new Command();
 program
-  .requiredOption('-h, --host <host>', 'Server host')
-  .requiredOption('-p, --port <port>', 'Server port')
-  .requiredOption('-c, --cache <path>', 'Cache directory path');
-
+ .requiredOption('-h, --host <host>', 'Server host')
+ .requiredOption('-p, --port <port>', 'Server port')
+ .requiredOption('-c, --cache <path>', 'Cache directory path');
 program.parse(process.argv);
 const { host, port, cache } = program.opts();
-
-// Виведення значень параметрів
 console.log(`Host: ${host}`);
 console.log(`Port: ${port}`);
 console.log(`Cache directory: ${cache}`);
-
-// Запуск веб-сервера
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Done\n');
+const server = http.createServer(async (req, res) => {
+ const urlParts = req.url.split('/');
+ const httpCode = urlParts[1];
+ const filePath = path.join(cache, `${httpCode}.jpg`);
+ if (req.method === 'GET') {
+ try {
+ const image = await fs.readFile(filePath);
+ res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+ res.end(image);
+ } catch (error) {
+ if (error.code === 'ENOENT') {
+ res.writeHead(404, { 'Content-Type': 'text/plain' });
+ res.end('Not Found');
+ } else {
+ res.writeHead(500, { 'Content-Type': 'text/plain' });
+ res.end('Internal Server Error');
+ }
+ }
+ } else {
+ res.writeHead(405, { 'Content-Type': 'text/plain' });
+ res.end('Method Not Allowed');
+ }
 });
-
-// Запуск сервера з вказаними параметрами хост і порт
 server.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}/`);
+ console.log(`http://${host}:${port}/`);
 });
